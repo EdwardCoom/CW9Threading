e4c6dacf2352466a29dd437d3bc1de96d5247916
Zach Coomer <zaccoom@ut.utm.edu>
2023-03-06 20:00:44 -0600

    Implemented base level of assignment

---
+++ CW9Threading/FindPiThread.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CW9Threading
+{
+    class FindPiThread
+    {
+        private int _numDarts;
+        private int _dartInsideCount;
+        private Random _random;
+
+        public FindPiThread(int numDarts) 
+        { 
+            _numDarts = numDarts;
+            _random = new Random();
+            _dartInsideCount = 0;
+        }
+
+        public int DartInsideCount { get => _dartInsideCount; }
+
+        public void throwDarts()
+        {
+            for (int i = 0; i < _numDarts; i++)
+            {
+                double x = _random.NextDouble();
+                double y = _random.NextDouble();
+
+                if (Math.Sqrt(x * x + y * y) <= 1)
+                    _dartInsideCount++;
+            }
+        }
+    }
+}
--- CW9Threading/Program.cs
+++ CW9Threading/Program.cs
@@ -1,15 +1,54 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace CW9Threading
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter the number of darts per thread: ");
+            int numDarts = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number of threads: ");
+            int numThreads = int.Parse(Console.ReadLine());
+                      
+            List<Thread> threads = new List<Thread>();
+            List<FindPiThread> findPiThreads = new List<FindPiThread>();
+
+
+
+            for (int i = 0; i < numThreads; i++)
+            {
+                FindPiThread piThread = new FindPiThread(numDarts);
+                findPiThreads.Add(piThread);
+
+                Thread thread = new Thread(new ThreadStart(piThread.throwDarts));
+                threads.Add(thread);
+
+                thread.Start();
+
+                Thread.Sleep(16);
+            }
+
+            foreach (Thread thread in threads) { thread.Join(); }
+
+            long count = 0;
+            foreach (FindPiThread piThread in findPiThreads) 
+            {
+                count += piThread.DartInsideCount;
+            }
+            
+            double pi = ((double)4 * (count) / (numDarts * numThreads));
+
+            Console.WriteLine("Pi = " + pi + "\nPress any key to close");
+
+            Console.ReadKey();
         }
     }
 }
